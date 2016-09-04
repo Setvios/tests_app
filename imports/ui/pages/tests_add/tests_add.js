385adbb6a87d46121ddce1f36031575efdd3a524
Setvios Set <setvios.it@gmail.com>
2016-09-04 03:40:25 +0300

    two pages added

---
+++ imports/ui/pages/tests_add/tests_add.js
@@ -0,0 +1,31 @@
+import { Template } from 'meteor/templating';
+import { Tests } from '../../../api/tests.js';
+import './tests_add.html';
+
+Template.tests_add.events({
+  'submit .add-tests-form'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const test = target.test.value;
+    const level = target.level.value;
+    const quantity = target.quantity.value;
+
+    // Insert a task into the collection
+    Tests.insert({
+      test,
+      level,
+      quantity,
+      createdAt: new Date(), // current time
+    }); 
+
+    // Clear form
+    target.test.value = '';
+    target.level.value = '';
+    target.quantity.value = '';
+
+    FlowRouter.go('/');
+  },  
+});
